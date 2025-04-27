3cfd5c32e1d068892554f96d3da6efb8a1cc2d78
aman godara <amangodara5686@gmail.com>
2025-04-27 15:44:32 +0530

    added loader

--- src/components/VideoGrid.jsx
+++ src/components/VideoGrid.jsx
@@ -21,35 +21,48 @@ export default function VideoGrid({
           transform: `translateX(-${startIndex * 272}px)`,
         }}
       >
-        {Array(totalCount)
-          .fill(null)
-          .map((_, idx) => {
-            const video = videos[idx];
-            return video ? (
+        {loading &&
+          Array(3)
+            .fill(null)
+            .map((_, idx) => (
               <div
-                key={video._id || idx}
-                className="w-64 flex-shrink-0 mx-2 cursor-pointer"
-                onClick={() => {
-                  setModalIndex(idx);
-                }}
+                key={idx}
+                className="w-64 flex-shrink-0 mx-2 flex items-center justify-center aspect-[9/16]"
               >
-                <VideoPreview
-                  containerRef={containerRef}
-                  src={baseUrl + video.src}
-                  title={video.title}
-                  likeCount={video.likeCount}
-                  liked={video.liked}
-                />
+                {console.log(idx)}
+                <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-white"></div>
               </div>
-            ) : (
-              <div key={idx}>
-                <SkeletonPreview
-                  loadMoreVideos={loadMoreVideos}
-                  loading={loading}
-                />
-              </div>
-            );
-          })}
+            ))}
+        {!loading &&
+          Array(totalCount)
+            .fill(null)
+            .map((_, idx) => {
+              const video = videos[idx];
+              return video ? (
+                <div
+                  key={video._id || idx}
+                  className="w-64 flex-shrink-0 mx-2 cursor-pointer"
+                  onClick={() => {
+                    setModalIndex(idx);
+                  }}
+                >
+                  <VideoPreview
+                    containerRef={containerRef}
+                    src={baseUrl + video.src}
+                    title={video.title}
+                    likeCount={video.likeCount}
+                    liked={video.liked}
+                  />
+                </div>
+              ) : (
+                <div key={idx}>
+                  <SkeletonPreview
+                    loadMoreVideos={loadMoreVideos}
+                    loading={loading}
+                  />
+                </div>
+              );
+            })}
 
         {loading && (
           <div className="w-64 flex-shrink-0 mx-2 flex items-center justify-center aspect-[9/16]">
